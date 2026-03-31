e9a9cc056f0bca0f829863a0b2d026241e0e6528
Ali Gates <hacktech877@gmail.com>
2026-03-31 10:29:21 +0500

    Code New

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
 app.listen(5000, () => {
